fde7e0d608a0db408e8dc59e712b7c014f2d97ee
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-03-06 16:51:00 +0100

    add static and template resources

---
+++ src/main/java/org/kedi/stripe/integration/stripeintegration/controllers/WebController.java
@@ -0,0 +1,14 @@
+package org.kedi.stripe.integration.stripeintegration.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        return "index";
+    }
+}
