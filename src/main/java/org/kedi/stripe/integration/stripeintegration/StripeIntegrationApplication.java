471f6397de6a77258ab9135daa00af4d74b1c597
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-03-06 20:48:02 +0100

    add my stripe api key from the stripe documentation

--- src/main/java/org/kedi/stripe/integration/stripeintegration/StripeIntegrationApplication.java
+++ src/main/java/org/kedi/stripe/integration/stripeintegration/StripeIntegrationApplication.java
@@ -1,5 +1,6 @@
 package org.kedi.stripe.integration.stripeintegration;
 
+import com.stripe.Stripe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,6 +10,11 @@ public class StripeIntegrationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StripeIntegrationApplication.class, args);
+
+
+        // This is your test secret API key.
+        Stripe.apiKey = "sk_test_51M6JLrEfacFPlTntFX30YVbOEIPvsOJwxwsJ4g4JciFQOwrP8mudiUZWCImCPkVbIEVJUud0bCC6ODJQDxn17RCY00ERonhHFU";
+
     }
 
 }
