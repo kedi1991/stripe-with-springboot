24270909980f79d8c5d45caddaf2194691a6563b
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-03-06 10:34:31 +0100

    add payment intent and dependencies for gson and stripe

--- src/main/java/org/kedi/stripe/integration/stripeintegration/controllers/PaymentController.java
+++ src/main/java/org/kedi/stripe/integration/stripeintegration/controllers/PaymentController.java
@@ -1,4 +1,32 @@
 package org.kedi.stripe.integration.stripeintegration.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class PaymentController {
+
+    @PostMapping("create-payment-intent")
+    public void CreatePaymentIntent(){
+
+
+            PaymentIntentCreateParams params =
+                    PaymentIntentCreateParams.builder()
+                            .setAmount(new Long(calculateOrderAmount(postBody.getItems())))
+                            .setCurrency("eur")
+                            // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+                            .setAutomaticPaymentMethods(
+                                    PaymentIntentCreateParams.AutomaticPaymentMethods
+                                            .builder()
+                                            .setEnabled(true)
+                                            .build()
+                            )
+                            .build();
+
+            // Create a PaymentIntent with the order amount and currency
+            PaymentIntent paymentIntent = PaymentIntent.create(params);
+
+            CreatePaymentResponse paymentResponse = new CreatePaymentResponse(paymentIntent.getClientSecret());
+            return gson.toJson(paymentResponse);
+
+
+    }
 }
