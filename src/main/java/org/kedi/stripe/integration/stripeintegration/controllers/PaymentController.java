c92b601b5dfdc98caaec71253ca1a16c7d8deafc
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-03-06 15:19:49 +0100

    correct return type of the payment controller

--- src/main/java/org/kedi/stripe/integration/stripeintegration/controllers/PaymentController.java
+++ src/main/java/org/kedi/stripe/integration/stripeintegration/controllers/PaymentController.java
@@ -1,16 +1,42 @@
 package org.kedi.stripe.integration.stripeintegration.controllers;
 
+import com.google.gson.annotations.SerializedName;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class PaymentController {
 
+    static class CreatePaymentItem {
+        @SerializedName("id")
+        String id;
+        public String getId() {
+            return id;
+        }
+    }
+    static class CreatePayment {
+        @SerializedName("items")
+        CreatePaymentItem[] items;
+        public CreatePaymentItem[] getItems() {
+            return items;
+        }
+    }
+
+    static class CreatePaymentResponse {
+        private String clientSecret;
+        public CreatePaymentResponse(String clientSecret) {
+            this.clientSecret = clientSecret;
+        }
+    }
+
+
     @PostMapping("create-payment-intent")
-    public void CreatePaymentIntent(){
-
-
+    public CreatePaymentResponse CreatePaymentIntent(@RequestBody CreatePayment createPayment) throws StripeException {
             PaymentIntentCreateParams params =
                     PaymentIntentCreateParams.builder()
-                            .setAmount(new Long(calculateOrderAmount(postBody.getItems())))
+                            .setAmount(1200L)
                             .setCurrency("eur")
                             // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
                             .setAutomaticPaymentMethods(
@@ -24,8 +50,7 @@ public class PaymentController {
             // Create a PaymentIntent with the order amount and currency
             PaymentIntent paymentIntent = PaymentIntent.create(params);
 
-            CreatePaymentResponse paymentResponse = new CreatePaymentResponse(paymentIntent.getClientSecret());
-            return gson.toJson(paymentResponse);
+            return new CreatePaymentResponse(paymentIntent.getClientSecret());
 
 
     }
