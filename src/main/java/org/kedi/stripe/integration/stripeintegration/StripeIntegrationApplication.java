e54cfa9e55b6f96163046782519de0332f99a28e
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-03-06 21:30:58 +0100

    add properties to the properties file and use field injection

--- src/main/java/org/kedi/stripe/integration/stripeintegration/StripeIntegrationApplication.java
+++ src/main/java/org/kedi/stripe/integration/stripeintegration/StripeIntegrationApplication.java
@@ -1,6 +1,8 @@
 package org.kedi.stripe.integration.stripeintegration;
 
 import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,12 +10,22 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 public class StripeIntegrationApplication {
 
+    @Value("${stripe.api.key}")
+    String apiKey;
+
+    @PostConstruct()
+    public void setup(){
+        Stripe.apiKey = apiKey;
+    }
+
     public static void main(String[] args) {
+
+
+
         SpringApplication.run(StripeIntegrationApplication.class, args);
 
 
-        // This is your test secret API key.
-        Stripe.apiKey = "sk_test_51M6JLrEfacFPlTntFX30YVbOEIPvsOJwxwsJ4g4JciFQOwrP8mudiUZWCImCPkVbIEVJUud0bCC6ODJQDxn17RCY00ERonhHFU";
+
 
     }
 
--- src/main/java/org/kedi/stripe/integration/stripeintegration/controllers/PaymentController.java
+++ src/main/java/org/kedi/stripe/integration/stripeintegration/controllers/PaymentController.java
@@ -1,9 +1,11 @@
 package org.kedi.stripe.integration.stripeintegration.controllers;
 
 import com.google.gson.annotations.SerializedName;
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
